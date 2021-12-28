e96841bc3e64ed915da9a28b1f574bf208ec7f98
Michael Abiona <amabiona21@gmail.com>
2021-12-28 16:08:10 +0100

    created pin upload ui

--- shareme_frontend/src/components/CreatePin.tsx
+++ shareme_frontend/src/components/CreatePin.tsx
@@ -15,13 +15,12 @@ const CreatePin = ({ user }: { user: User | null }) => {
   const [about, setAbout] = useState('');
   const [destination, setDestination] = useState('');
   const [loading, setLoading] = useState(false);
-  const [fields, setFields] = useState(null);
-  const [category, setCategory] = useState(null);
+  const [fields, setFields] = useState<boolean | null>(null);
+  const [category, setCategory] = useState<string | null>(null);
   const [imageAsset, setImageAsset] = useState<SanityImageAssetDocument | null>(null);
   const [wrongImageType, setWrongImageType] = useState(false);
 
   const uploadImage = (e: ChangeEvent<HTMLInputElement>) => {
-
     if(e.target.files) {
       const { type, name } = e.target.files[0];
 
@@ -43,13 +42,48 @@ const CreatePin = ({ user }: { user: User | null }) => {
     }
   }
 
+  const savePin = () => {
+    if(title && about && destination && imageAsset?._id && category) {
+      const doc = {
+        _type: 'pin',
+        title,
+        about,
+        destination,
+        image: {
+          _type: 'image',
+          asset: {
+            _type: 'reference',
+            _ref: imageAsset._id
+          }
+        },
+        userId: user?._id,
+        postedBy: {
+          _type: 'postedBy',
+          _ref: user?._id,
+        },
+        category
+      }
+
+      client.create(doc)
+        .then(() => {
+          navigate('/')
+        })
+    } else {
+      setFields(true);
+
+      setTimeout(() => {
+        setFields(false);
+      }, 2000);
+    }
+  }
+
   return (
     <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
       {fields && (
         <p className="text-red-500 p-5 text-xl transition-all duration-150 ease-in">Pls fill in all the fields</p>
       )}
       <div className="flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 p-3 w-full lg:w-4/5">
-        <div className="bg-secondaryColor p-3 flex flex-0.7 w-4">
+        <div className="bg-secondaryColor p-3 flex flex-0.7 w-full">
           <div className="flex justify-center items-center flex-col border-dotted border-2 border-gray-300 p-3 w-full h-420">
             {loading && (<Spinner />)}
             {wrongImageType && (<p>Wrong image type</p>)}
@@ -62,7 +96,7 @@ const CreatePin = ({ user }: { user: User | null }) => {
                     </p>
                     <p className="text-lg">Click to upload</p>
                   </div>
-                  <p className="mt-32 text-gray-400 text-xs md:text-sm lg:text-base">Use high-quality JPG, SVG, PNG or GIF less than 20MB</p>
+                  <p className="mt-32 text-gray-400">Recommendation: Use high-quality JPG,JPEG,SVG,PNG or GIF less than 20MB</p>
                 </div>
 
                 <input type="file" name="upload-image" onChange={uploadImage} className="w-0 h-0" />
@@ -90,6 +124,45 @@ const CreatePin = ({ user }: { user: User | null }) => {
             placeholder="Add your title here"
             className="outline-none text-2xl sm:text-3xl font-bold border-b-2 border-gray-200 p-2"
           />
+          {user && (
+            <div className="flex gap-2 my-2 items-center bg-white rounded-lg">
+              <img src={user.image} className="w-10 h-10 rounded-full" alt="user-profile" />
+              <p className="font-bold">{user.userName}</p>
+            </div>
+          )}
+          <input  
+            type="text"  
+            value={about}
+            onChange={(e) => setAbout(e.target.value)}
+            placeholder="What is your pin about?"
+            className="outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2"
+          />
+          <input  
+            type="text"  
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            placeholder="Add a destination link "
+            className="outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2"
+          />
+          <div className="flex flex-col">
+            <div>
+              <p className="mb-2 font-semibold text-lg sm:text-xl">Choose pin category</p>
+              <select 
+                onChange={(e) => setCategory(e.target.value)} 
+                className="outline-none w-4/5 text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer"
+              >
+                <option value="other" className="bg-white">Select Category</option>
+                {categories.map((category) => (
+                  <option value={category.name} className="text-base border-0 outline-none capitalize bg-white text-black">{category.name}</option>
+                ))}
+              </select>
+            </div>
+            <div className="flex justify-end items-end mt-5">
+              <button type="button" onClick={savePin} className="bg-red-500 text-white font-bold p-2 rounded-full w-28 outline-none">
+                Save Pin
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
